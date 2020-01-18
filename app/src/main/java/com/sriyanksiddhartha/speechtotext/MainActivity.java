38ba2e1cac3caed0e84f5dfd024605b423d64038
Sunil <sunil.pillai16@siesgst.ac.in>
2020-01-18 23:21:16 +0530

    mock sih
    clg

--- app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
@@ -5,22 +5,39 @@ import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.regex.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
-	private TextView txvResult;
+
+	EditText sres,patient_details,symptoms,diagnosis,prescription,advice;
+	Button btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		txvResult = (TextView) findViewById(R.id.txvResult);
-	}
+		sres   = (EditText)findViewById(R.id.sres);
+		btn   = (Button) findViewById(R.id.button);
+		patient_details = (EditText)findViewById(R.id.patient_details);
+		symptoms = (EditText)findViewById(R.id.editText9);
+		diagnosis = (EditText)findViewById(R.id.editText10);
+		prescription =(EditText)findViewById(R.id.editText8);
+
+
+
+
+
+		}
+
+
 
 	public void getSpeechInput(View view) {
 
@@ -35,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
@@ -43,9 +62,115 @@ public class MainActivity extends AppCompatActivity {
 			case 10:
 				if (resultCode == RESULT_OK && data != null) {
 					ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-					txvResult.setText(result.get(0));
+					sres.append(" "+result.get(0));
+
 				}
 				break;
 		}
+
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String text = String.valueOf(sres.getText());
+
+				Pattern pattern = Pattern.compile("(?<=patient details).*.(?=symptoms)");
+
+				Matcher matcher = pattern.matcher(text);
+
+				boolean found = false;
+				while (matcher.find())
+				{
+					String result = matcher.group().toString();
+					result.replace("symptoms",".");
+					patient_details.setText(result);
+					sres.append(result);
+					found = true;
+				}
+				if (!found)
+				{
+					sres.append("\nI didn't find the text");
+				}
+
+
+
+				pattern = Pattern.compile("(?<=symptoms).*.(?=diagnosis)");
+
+				matcher = pattern.matcher(text);
+
+				found = false;
+				while (matcher.find())
+				{
+					String result = matcher.group().toString();
+					result.replace("diagnosis",".");
+					symptoms.setText(result);
+					sres.append(result);
+					found = true;
+				}
+				if (!found)
+				{
+					sres.append("\nI didn't find the text");
+				}
+
+
+
+				pattern = Pattern.compile("(?<=diagnosis).*.(?=prescription)");
+
+				matcher = pattern.matcher(text);
+
+				found = false;
+				while (matcher.find())
+				{
+					String result = matcher.group().toString();
+					result.replace("prescription",".");
+					diagnosis.setText(result);
+					sres.append(result);
+					found = true;
+				}
+				if (!found)
+				{
+					sres.append("\nI didn't find the text");
+				}
+
+
+				pattern = Pattern.compile("(?<=prescription).*.(?=advice|advise)");
+
+				matcher = pattern.matcher(text);
+
+				found = false;
+				while (matcher.find())
+				{
+					String result = matcher.group().toString();
+					result.replace("advice",".");
+					prescription.setText(result);
+					sres.append(result);
+					found = true;
+				}
+				if (!found)
+				{
+					sres.append("\nI didn't find the text");
+				}
+
+
+				pattern=Pattern.compile("(?<=advice|advise).*");
+
+				matcher=pattern.matcher(text);
+
+				found = false;
+				while (matcher.find())
+				{
+					String result = "Advice : "+matcher.group().toString();
+					prescription.append("\n\n"+result);
+					sres.append(result);
+					found = true;
+				}
+				if (!found)
+				{
+					sres.append("\nI didn't find the text");
+				}
+
+
+
+			}
+		});
 	}
 }
